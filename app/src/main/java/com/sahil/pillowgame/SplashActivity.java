2405b723b399b388191d13a3143892410269e509
Aaryan Pandey <54701470+imaaryan@users.noreply.github.com>
2024-09-06 02:09:50 +0530

    First Change
    I have updated the logo, splash screen and first page ui

---
+++ app/src/main/java/com/sahil/pillowgame/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.sahil.pillowgame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DISPLAY_LENGTH = 3000; // Splash screen delay time in milliseconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start MainActivity after splash screen
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
